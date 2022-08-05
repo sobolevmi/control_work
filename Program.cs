5790502c8dc5bd5b4c13b2505e75959ac80699f8
Maksim Sobolev <sobmig@gmail.com>
2022-08-05 05:01:49 +0300

    создан метод по созданию нового массива из элементов, длина которых меньше либо равна 3 символам

--- Program.cs
+++ Program.cs
@@ -1,11 +1,27 @@
-﻿// Программа, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа
+﻿// Программа, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символам
+
 string [] CreateUserArray (int n)
+// метод по заполнению первоначального массива с клавиатуры пользователем
 {
     string [] userArray = new string [n];
     for (int index = 0; index < userArray.Length; index++)
     {
-        Console.Write ("Введите элемент строкового массива: ");     // метод по заполнению первоначального массива с клавиатуры пользователем
+        Console.Write ("Введите элемент строкового массива: ");     
         userArray [index] = Console.ReadLine ();
     }
     return userArray;
+}
+
+string [] CreateNewArrayFrom3SymbolsElements (string [] anyArray, int count) 
+// метод по созданию нового массива из элементов, длина которых меньше либо равна 3 символам
+{
+    string [] newArray = new string [count];
+    int k = 0;
+    for (int j = 0; j < anyArray.Length; j++)
+    {
+        if (anyArray[j].Length <= 3)                                    
+        newArray [k] = anyArray [j];
+        k++;
+    }
+    return newArray;
 }
