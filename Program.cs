3cb8b791389096310895d0e85dbc51a28ea85cf8
Maksim Sobolev <sobmig@gmail.com>
2022-08-05 04:58:45 +0300

    создан метод по заполнению первоначального массива

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа
+string [] CreateUserArray (int n)
+{
+    string [] userArray = new string [n];
+    for (int index = 0; index < userArray.Length; index++)
+    {
+        Console.Write ("Введите элемент строкового массива: ");     // метод по заполнению первоначального массива с клавиатуры пользователем
+        userArray [index] = Console.ReadLine ();
+    }
+    return userArray;
+}
